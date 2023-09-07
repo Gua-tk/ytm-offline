bd4137d294140c4a1967fb1b67eaa21e5f9d7c62
AleixMT <aleixaretra@gmail.com>
2023-09-08 00:36:19 +0200

    UPDATED: global routes configured, moved main to the app folder

--- src/app/controllers/global_routes.py
+++ src/app/controllers/global_routes.py
@@ -2,18 +2,16 @@
 # -*- coding: utf-8 -*-
 import uuid
 import os
-from importlib.resources import Resource
 
 from flask import Blueprint
 from flask import request, send_file
-from flask_restx import Api
+from flask_restx import Api, Resource
 
-from src.app.AppSingleton import app
 from src.app.services.PlaylistService import PlaylistService
 from src.app.services.VideoService import VideoService
 from src.app.services.CompressionService import CompressionService
 
-global_bp = Blueprint('global', __name__, url_prefix='/global')
+global_bp = Blueprint('global', __name__, url_prefix='/api')
 api = Api(global_bp, doc='/doc', title='Global REST API', version='1.0')
 
 videoService = VideoService()
@@ -24,68 +22,66 @@ root_path = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.p
 download_path = os.path.join(root_path, "downloads")
 
 
-@api.route('/')
-class Global:
-    def __init__(self):
-        pass
-
-    #@app.route('/downloadPlaylist', methods=['POST'])
-    def download_playlist(self):
-        """
-        Downloads a playlist from youtube as a set of .mp3 files compressed into a single .zip file.
-        :return: a .zip file containing all the .mp3 files of the supplied playlist.
-        """
-        data = request.json
-        playlist_url = data['playlist_url']
-        request_uuid = uuid.uuid4()
-        current_download_path = os.path.join(download_path, "playlists", str(request_uuid))
-        directory_name = playlistService.download_audio_playlist(playlist_url, current_download_path)
-        compressionService.zip_folder(os.path.join(current_download_path, directory_name), os.path.join(current_download_path, directory_name + ".zip"))
-        return send_file(os.path.join(current_download_path, directory_name + ".zip"), as_attachment=True)
-
-    #@app.route('/downloadAudio', methods=['POST'])
-    def download_audio(self):
-        data = request.json
-        audio_url = data['audio_url']
-        request_uuid = uuid.uuid4()
-        current_download_path = os.path.join(download_path, "audios", str(request_uuid))
-        file_name = videoService.download_audio(audio_url, current_download_path)
-        return send_file(os.path.join(current_download_path, file_name), as_attachment=True)
-
-    #@app.route('/uploadAudio', methods=['POST'])
-    def upload_audio(self):
-        data = request.json
-        audio_url = data['audio_url']
-        request_uuid = uuid.uuid4()
-        current_download_path = os.path.join(download_path, "audios", str(request_uuid))
-        file_name = videoService.download_audio(audio_url, current_download_path)
-        return str(playlistService.upload_audio(os.path.join(current_download_path, file_name)))
-
-    #@app.route('/uploadPlaylist', methods=['POST'])
-    def upload_playlist(self):
-        data = request.json
-        playlist_url = data['playlist_url']
-        request_uuid = uuid.uuid4()
-        current_download_path = os.path.join(download_path, "playlists", str(request_uuid))
-        directory_name = playlistService.download_audio_playlist(playlist_url, current_download_path)
-        print("DOWNLOADING FINISHED. STARTING UPLOAD<")
-        upload_infos = playlistService.upload_playlist(os.path.join(current_download_path, directory_name))
-        return "\n".join(upload_infos)
-
-    #@app.route('/uploadToPlaylist', methods=['POST'])
-    def upload_to_playlist(self):
-        data = request.json
-        playlist_url = data['playlist_url']
-        request_uuid = uuid.uuid4()
-        current_download_path = os.path.join(download_path, "playlists", str(request_uuid))
-        directory_name = playlistService.download_audio_playlist(playlist_url, current_download_path)
-        upload_infos = playlistService.upload_playlist(os.path.join(current_download_path, directory_name))
-        title, description = playlistService.download_playlist_data(playlist_url)
-        playlistService.save_playlist({'title': title, 'description': description})
-
-    #@app.route('/', methods=['GET'])
-    def hello(self):
-        return 'Holaaaa MAMAMAA'
+@global_bp.route('/downloadPlaylist', methods=['POST'])
+def download_playlist():
+    """
+    Downloads a playlist from youtube as a set of .mp3 files compressed into a single .zip file.
+    :return: a .zip file containing all the .mp3 files of the supplied playlist.
+    """
+    data = request.json
+    playlist_url = data['playlist_url']
+    request_uuid = uuid.uuid4()
+    current_download_path = os.path.join(download_path, "playlists", str(request_uuid))
+    directory_name = playlistService.download_audio_playlist(playlist_url, current_download_path)
+    compressionService.zip_folder(os.path.join(current_download_path, directory_name),
+                                  os.path.join(current_download_path, directory_name + ".zip"))
+    return send_file(os.path.join(current_download_path, directory_name + ".zip"), as_attachment=True)
 
 
+@global_bp.route('/downloadAudio', methods=['POST'])
+def download_audio():
+    data = request.json
+    audio_url = data['audio_url']
+    request_uuid = uuid.uuid4()
+    current_download_path = os.path.join(download_path, "audios", str(request_uuid))
+    file_name = videoService.download_audio(audio_url, current_download_path)
+    return send_file(os.path.join(current_download_path, file_name), as_attachment=True)
 
+
+@global_bp.route('/uploadAudio', methods=['POST'])
+def upload_audio():
+    data = request.json
+    audio_url = data['audio_url']
+    request_uuid = uuid.uuid4()
+    current_download_path = os.path.join(download_path, "audios", str(request_uuid))
+    file_name = videoService.download_audio(audio_url, current_download_path)
+    return str(playlistService.upload_audio(os.path.join(current_download_path, file_name)))
+
+
+@global_bp.route('/uploadPlaylist', methods=['POST'])
+def upload_playlist():
+    data = request.json
+    playlist_url = data['playlist_url']
+    request_uuid = uuid.uuid4()
+    current_download_path = os.path.join(download_path, "playlists", str(request_uuid))
+    directory_name = playlistService.download_audio_playlist(playlist_url, current_download_path)
+    print("DOWNLOADING FINISHED. STARTING UPLOAD<")
+    upload_infos = playlistService.upload_playlist(os.path.join(current_download_path, directory_name))
+    return "\n".join(upload_infos)
+
+
+@global_bp.route('/uploadToPlaylist', methods=['POST'])
+def upload_to_playlist():
+    data = request.json
+    playlist_url = data['playlist_url']
+    request_uuid = uuid.uuid4()
+    current_download_path = os.path.join(download_path, "playlists", str(request_uuid))
+    directory_name = playlistService.download_audio_playlist(playlist_url, current_download_path)
+    upload_infos = playlistService.upload_playlist(os.path.join(current_download_path, directory_name))
+    title, description = playlistService.download_playlist_data(playlist_url)
+    playlistService.save_playlist({'title': title, 'description': description})
+
+
+@global_bp.route('/hello', methods=['GET'])
+def hello():
+    return 'hello world'
