1f9da835ac0eba9e2d5e2b321cb6d53b3012791c
AleixMT <aleixaretra@gmail.com>
2023-09-30 03:57:52 +0200

    UPDATED: authentication header for test file

--- src/test/test_routes.sh
+++ src/test/test_routes.sh
@@ -11,22 +11,22 @@ test_secure_hello()
 
 test_download_playlist()
 {
-  curl -X POST -H "Content-Type: application/json" localhost:5000/api/global/downloadPlaylist -d "{\"playlist_url\": \"$1\"}" --output "${DOWNLOADS_TEST_DIR}/playlist.zip"
+  curl -X POST -H "Content-Type: application/json" localhost:5000/api/global/downloadPlaylist -d "{\"playlist_url\": \"$1\"}" --output "${DOWNLOADS_TEST_DIR}/playlist.zip" -u "ytm@ytm.com:changeit"
 }
 
 test_download_audio()
 {
-  curl -X POST -H "Content-Type: application/json" localhost:5000/api/global/downloadAudio -d "{\"audio_url\": \"https://www.youtube.com/watch?v=dQw4w9WgXcQ\"}" --output "${DOWNLOADS_TEST_DIR}/audio.mp3"
+  curl -X POST -H "Content-Type: application/json" localhost:5000/api/global/downloadAudio -d "{\"audio_url\": \"https://www.youtube.com/watch?v=dQw4w9WgXcQ\"}" --output "${DOWNLOADS_TEST_DIR}/audio.mp3" -u "ytm@ytm.com:changeit"
 }
 
 test_upload_audio()
 {
-  curl -X POST -H "Content-Type: application/json" localhost:5000/api/global/uploadAudio -d "{\"audio_url\": \"https://www.youtube.com/watch?v=e9dZQelULDk\"}"
+  curl -X POST -H "Content-Type: application/json" localhost:5000/api/global/uploadAudio -d "{\"audio_url\": \"https://www.youtube.com/watch?v=e9dZQelULDk\"}" -u "ytm@ytm.com:changeit"
 }
 
 test_upload_playlist()
 {
-  curl -X POST -H "Content-Type: application/json" localhost:5000/api/global/uploadPlaylist -d "{\"playlist_url\": \"$1\"}"
+  curl -X POST -H "Content-Type: application/json" localhost:5000/api/global/uploadPlaylist -d "{\"playlist_url\": \"$1\"}" -u "ytm@ytm.com:changeit"
 }
 
 test_login()
@@ -44,13 +44,13 @@ TEST_DIR=$(dirname "$(realpath "$0")")
 DOWNLOADS_TEST_DIR="${TEST_DIR}/downloads"
 mkdir -p "${TEST_DIR}/downloads"
 
-#test_secure_hello
+test_secure_hello
 #test_hello
 #test_download_playlist https://www.youtube.com/playlist?list=PLsS6gopj1BA1WYUc04ieKU2Z7cH893sQ7
-#test_download_playlist https://youtu.be/3tw2P65wv5E?si=nmZ7QaRCZb5yg-MY
-#test_download_audio
+test_download_playlist "https://youtu.be/3tw2P65wv5E?si=nmZ7QaRCZb5yg-MY"
+test_download_audio
 #test_upload_audio
 #test_upload_playlist https://www.youtube.com/playlist?list=PLsS6gopj1BA1WYUc04ieKU2Z7cH893sQ7
 #test_upload_playlist https://www.youtube.com/watch?v=3tw2P65wv5E&list=PLvEI0iOxif017-fv4P0ApFmzRI-vWBPvb
 #test_login "ytmt@ytm.com" "changeit"
-test_signup "ytmt@ytm.com" "changeit"
+#test_signup "ytmt@ytm.com" "changeit"
